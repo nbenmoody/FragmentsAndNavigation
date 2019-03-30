f85a3060ed255c3ba555b7b337192d16fc699b52
Ben Moody <nathanielmoody@live.com>
2019-03-30 19:59:09 -0400

    Finish wiring up click-clisteners and nav controllers.

--- app/src/main/java/com/nbmoody/fragmentation/FragmentTheSecond.kt
+++ app/src/main/java/com/nbmoody/fragmentation/FragmentTheSecond.kt
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_the_second.*
 
 
 class FragmentTheSecond : Fragment() {
@@ -15,6 +18,14 @@ class FragmentTheSecond : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_the_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        secondButton.setOnClickListener {
+            findNavController(nav_host_fragment).navigate(R.id.action_fragmentTheSecond_to_fragmentTheThird)
+        }
     }
 
 
--- app/src/main/java/com/nbmoody/fragmentation/FragmentTheThird.kt
+++ app/src/main/java/com/nbmoody/fragmentation/FragmentTheThird.kt
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_the_third.*
 
 
 class FragmentTheThird : Fragment() {
@@ -15,6 +18,14 @@ class FragmentTheThird : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_the_third, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        thirdButton.setOnClickListener {
+            findNavController(nav_host_fragment).navigate(R.id.action_fragmentTheThird_to_fragmentTheFirst)
+        }
     }
 
 
